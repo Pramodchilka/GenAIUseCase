76a0ddf89378edd6797212b571aff6b4c1c61a49
Pramodchilka <45305929+Pramodchilka@users.noreply.github.com>
2025-06-30 19:22:58 -0600

    Update validate_content.py

--- validate_content.py
+++ validate_content.py
@@ -19,7 +19,8 @@ s3 = boto3.client(
 
 # S3 Configuration
 BUCKET_NAME = "cicd-validation-media"
-PREFIX = "Valid_Files/"
+# PREFIX = "Valid_Files/"
+PREFIX = "Error_Files/"
 THRESHOLD = 0.8
 categories = ["educational", "entertainment", "sports", "news", "documentary"]
 report_lines = []
